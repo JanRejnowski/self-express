6d7350bbad984f76906cc5ad26c44adf40beb36d
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-11-06 10:58:24 +0100

    Initial commit

---
+++ self-express-backend/src/main/java/com/example/selfexpressbackend/SelfExpressBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfexpressbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfExpressBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfExpressBackendApplication.class, args);
+    }
+
+}
---
+++ self-express-backend/src/test/java/com/example/selfexpressbackend/SelfExpressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfexpressbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfExpressBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
