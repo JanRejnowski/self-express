c5f808573b10515efdd7959654d2930545730d92
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-11-11 22:06:17 +0100

    Added AuthController;

---
+++ self-express-backend/src/main/java/com/example/selfexpressbackend/controllers/AuthController.java
@@ -0,0 +1,112 @@
+package com.example.selfexpressbackend.controllers;
+
+import com.example.selfexpressbackend.models.Role;
+import com.example.selfexpressbackend.models.RoleEnum;
+import com.example.selfexpressbackend.models.User;
+import com.example.selfexpressbackend.payload.request.LoginRequest;
+import com.example.selfexpressbackend.payload.request.RegisterRequest;
+import com.example.selfexpressbackend.payload.response.JwtResponse;
+import com.example.selfexpressbackend.payload.response.MessageResponse;
+import com.example.selfexpressbackend.repository.RoleRepository;
+import com.example.selfexpressbackend.repository.UserRepository;
+import com.example.selfexpressbackend.security.jwt.JwtUtils;
+import com.example.selfexpressbackend.security.services.AppUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        AppUserDetails appUserDetails = (AppUserDetails) authentication.getPrincipal();
+        List<String> roles = appUserDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt, appUserDetails.getId(),
+                                                      appUserDetails.getUsername(),
+                                                      appUserDetails.getEmail(),
+                                                      roles));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
+        if (userRepository.existsByUsername(registerRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already in use!"));
+        }
+
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        User user = new User(registerRequest.getUsername(),
+                             registerRequest.getEmail(),
+                             passwordEncoder.encode(registerRequest.getPassword()));
+
+        Set<String> stringRoles = registerRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (stringRoles == null) {
+            Role userRole = roleRepository.findByName(RoleEnum.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+            roles.add(userRole);
+        } else {
+            stringRoles.forEach(role -> {
+                if ("admin".equals(role)) {
+                    Role adminRole = roleRepository.findByName(RoleEnum.ROLE_ADMIN)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(adminRole);
+                } else {
+                    Role userRole = roleRepository.findByName(RoleEnum.ROLE_USER)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
