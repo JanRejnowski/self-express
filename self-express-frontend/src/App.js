b9437b6c7388626075b4b30b083680d49dbd3890
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-11-13 00:20:52 +0100

    Added navbar project in App.js

--- self-express-frontend/src/App.js
+++ self-express-frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Navbar bg="light" sticky="top" expand="lg">
+          <Navbar.Brand>
+            ExpressYourself
+          </Navbar.Brand>
+
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link hred="/user">User</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/admin">Admin Board</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
